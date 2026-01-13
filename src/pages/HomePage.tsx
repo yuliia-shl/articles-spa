4cdbfdb87af9b5b888617bc688a7d888d0cc7890
Yuli <yu.expert@gmail.com>
2026-01-13 19:47:16 +0200

    chore: set up project structure and base components

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,13 @@
+import ArticlesList from "../components/ArticlesList/ArticlesList";
+import SearchInput from "../components/SearchInput/SearchInput";
+
+const HomePage = () => {
+  return (
+    <>
+      <SearchInput />
+      <ArticlesList />
+    </>
+  );
+};
+
+export default HomePage;
